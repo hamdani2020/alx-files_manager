e4c1f8d31ebae28ecd0dc64d1f42417f735a79c6
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-03-13 03:14:36 +0000

    Fixed ESlint

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import startServer from './libs/boot';
 import express from 'express';
+import startServer from './libs/boot';
 
 import injectRoutes from './routes';
 import injectMiddlewares from './libs/middlewares';

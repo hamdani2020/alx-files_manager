a511c9aabb4cc17753d582b463fca7fa5e446748
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-03-13 03:41:01 +0000

    Fixed Eslint

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class AuthController {
   static async getConnect(req, res) {
     const { user } = req;
     const token = uuidv4();
-
 
     await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
     res.status(200).json({ token });
